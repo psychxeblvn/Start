5a4a571c2b8a34956476eb9c0fba0da5df0d32cd
psychxeblvn <jojoykin@gmail.com>
2024-02-20 21:55:55 +0400

    commit1

---
+++ src/Figure.java
@@ -0,0 +1,12 @@
+public class Figure {
+
+    public void name4(int side) {
+        System.out.println("это четырёхугольник");
+    }
+
+    public void Name3() {
+        System.out.println("это треугольник");
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Figure figure = new Figure();
+        Rectangle rectangle = new Rectangle();
+        Square square = new Square(10);
+        Triangle triangle = new Triangle();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        square.perimeterOfSquare(10);
+        square.AreaOfSquare(10);
+        triangle.perimeterOfTriangle(10,10,10);
+        triangle.AreaOfTriangle(10,10,10);
     }
 }
